abfef0a53de17d3103d7d7d003085fdd1fe6ac9d
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-05-12 18:24:13 -0400

    Life cycles hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,42 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterContentChecked, AfterViewChecked{
   title = 'projeto-1';
+  
+  constructor(){
+    console.log("Component constructor")
+  }
+  ngAfterContentChecked(): void {
+    console.log("Component Content Checked")
+  }
+  ngAfterViewChecked(): void {
+    console.log("Component View Checked")
+  }
+  ngOnDestroy(): void {
+    console.log("Component Content Destroy")
+  }
+  ngAfterContentInit(): void {
+    console.log("Component Content Init")
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Change")  
+  }
+  
+  ngOnInit(): void {
+    console.log('Component OnInit')
+    this.title = 'Novo titulo, de novo'
+  }
+  
+  ngDoCheck(): void {
+    console.log("Component DoCheck")
+  }
+
+  mudarTexto(){
+    for (let i = 0; i < 20; i++) {
+      this.title += " De novo"
+    }
+  }
+
 }
